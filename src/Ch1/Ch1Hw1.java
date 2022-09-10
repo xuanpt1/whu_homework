e2b08b7c20d1ac0a28c391194616918a2b387417
xuanpt1 <bktncn7m@gmail.com>
2022-09-11 03:45:03 +0800

    add：第一次作业内容

---
+++ src/Ch1/Ch1Hw1.java
@@ -0,0 +1,12 @@
+package Ch1;
+
+public class Ch1Hw1 {
+    //类名，Java一个.java文件只能有一个public class，且与文件名相同
+    public static void main(String[] args) {
+        //main函数
+        System.out.println("Hello Java");
+        //系统控制台格式化输出
+
+        //Java void可无return
+    }
+}
---
+++ src/Ch1/Ch1Hw2.java
@@ -0,0 +1,10 @@
+package Ch1;
+
+public class Ch1Hw2 {
+    public static void main(String[] args) {
+        System.out.println("姓名：熊帅");
+        System.out.println("性别：男");
+        System.out.println("籍贯：湖北");
+        System.out.println("住址：国软c3");
+    }
+}
