38ac86e620a464c5e4cdee7054049ea85917ac61
xuanpt1 <bktncn7m@gmail.com>
2022-09-21 14:54:54 +0800

    add：第三章作业内容

--- src/Ch3/Ch3Hw1.java
+++ src/Ch3/Ch3Hw1.java
@@ -3,8 +3,8 @@ package Ch3;
 public class Ch3Hw1 {
     public static void main(String[] args) {
         int sumOfOdd=0;
-        for(int number=0;number<=100;number++){
-            if(number%2!=0){sumOfOdd+=number;}
+        for(int i=0;i<=100;i++){
+            if(i%2!=0){sumOfOdd+=i;}
         }
         System.out.println("The Sum Of Odd Of 1 to 100 is "+sumOfOdd);
     }
--- src/Ch3/Ch3Hw2.java
+++ src/Ch3/Ch3Hw2.java
@@ -4,9 +4,9 @@ public class Ch3Hw2 {
     public static void main(String[] args) {
         int sum=0;
         int count=0;
-        for(int number=0;number<=100;number++){
-            if(number % 7 == 0){
-                sum+=number;
+        for(int i=0;i<=100;i++){
+            if(i % 7 == 0){
+                sum+=i;
                 count++;
             }
         }
